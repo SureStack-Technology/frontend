b6d65e557319006ee5bf30e2c3d5315d69685d93
Mark Devops <mark@wakadigital.co.nz>
2026-04-08 12:36:53 +1200

    sitemap update

---
+++ studio/ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'sanity-studio',
+      script: 'npm',
+      args: 'run start',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import Sitemap from 'vite-plugin-sitemap';
+import { createHtmlPlugin } from 'vite-plugin-html';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({  
   plugins: [
+    react(),
+    Sitemap({
+      baseUrl: 'https://surestack.tech',
+      generateRobotsTxt: true,
+    }),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          title: 'SureStack Technology',
+          description: 'Risk Analysis for Crypto',
+        },
+      },
+    }),
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
 })
